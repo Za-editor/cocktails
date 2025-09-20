2b56bff0a7a32f667ec24008ffa1552a81b1875a
Fawaz Tajudeen <flexkid02@gmail.com>
2025-09-20 13:17:53 +0100

    Fixed the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const ismobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -37,6 +40,27 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = ismobile ? "top 50%" : "center 60%";
+    const endValue = ismobile ? "120% top" : "bottom top";
+
+    //Video Animation timeline
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+            scrub: true,
+        pin:true,
+
+      },
+    })
+
+      videoRef.current.onloadedmetadata = () => {
+          tl.to(videoRef.current, {
+            currentTime: videoRef.current.duration
+        })  
+      }
   }, []);
   return (
     <>
@@ -72,6 +96,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+                  preload="auto"
+
+        />
+      </div>
     </>
   );
 };
